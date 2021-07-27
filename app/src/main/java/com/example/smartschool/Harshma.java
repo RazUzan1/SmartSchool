51a89831fb7e3b6fcceebfe630fabe794a497e8b
razuzan303@gmail.com <razuzan303@gmail.com>
2021-07-27 20:39:24 +0300

    Initial commit

---
+++ app/src/main/java/com/example/smartschool/Harshma.java
@@ -0,0 +1,14 @@
+package com.example.smartschool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Harshma extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_harshma);
+    }
+}
