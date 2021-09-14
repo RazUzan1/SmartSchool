4fa6178dabebcf313fbc2066f5666afe61a13bb1
razuzan303@gmail.com <razuzan303@gmail.com>
2021-09-14 19:42:43 +0300

    עברתי עם ג'פרי על השגיאות כרגע הכול תקין נשאר רק לעבוד על העיצוב

--- app/src/main/java/com/example/smartschool/Harshma.java
+++ app/src/main/java/com/example/smartschool/Harshma.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class Harshma extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
